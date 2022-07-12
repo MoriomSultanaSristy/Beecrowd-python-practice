000f6906e1f105e87e0efdc2a101acaf70b23395
MoriomSultanaSristy <107357072+MoriomSultanaSristy@users.noreply.github.com>
2022-07-13 00:01:40 +0600

    using variables

---
+++ variable.py
@@ -0,0 +1,8 @@
+character_name ="John"
+character_age ="45"
+print ("There was a man named "+character_name+",")
+print ("He was "+character_age+" years old")
+character_name ="El"
+character_age ="12"
+print ("He really likes the name "+character_name)
+print("But he didn't like being "+character_age)
