f0492c2ddb14b7e95eac68e4fb8ce0d8751a9a79
MoriomSultanaSristy <107357072+MoriomSultanaSristy@users.noreply.github.com>
2022-07-13 10:07:22 +0600

    strings

---
+++ workingwithStrings.py
@@ -0,0 +1,11 @@
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe","Elephant"))
+print(len(phrase))
+print(phrase.index("Acad"))
+print(phrase[0])
+print(phrase.index("G"))
+print(phrase+" is cool")
+
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.lower().islower())
