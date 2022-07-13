60a7438088422406f6a3ba28a0708b69ce94f6f6
MoriomSultanaSristy <107357072+MoriomSultanaSristy@users.noreply.github.com>
2022-07-13 12:55:43 +0600

    listing index names

---
+++ list.py
@@ -0,0 +1,8 @@
+friends = ["Kavein","karen","jim","Oscar","Toby","Joyes"]
+friends[1]="El"
+friends[0]="Mike"
+friends[2]="Dustin"
+friends[3]="Max"
+friends[4]="Will"
+friends[5]="lucas"
+print(friends[0:6])
